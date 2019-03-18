0fe5de7aab04795b8ab1abf92bcea8a560d8947a
ricksterhd123 <ricksterhd123@gmail.com>
2019-03-18 12:06:29 +0000

    Working on preventing camera collisions beyond the near plane with GameObjects.

---
+++ Assets/Scenes/CameraScript.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+class CameraScript : MonoBehaviour
+{
+    // Camera movement speed factor
+    public float moveSpeed { get; set; } = 0.5f;
+    // Camera rotation speed factor
+    public float rotateSpeed { get; set; } = 1.5f;
+    public String forwardBtnName { get; set; } = "Forward";
+    // Axis name for moving forward
+    public String backwardBtnName { get; set; } = "Back";
+    // Axis name for moving back
+    public String leftBtnName { get; set; } = "Left";
+    // Axis for moving right
+    public String rightBtnName { get; set; } = "Right";
+    // Main camera object
+    public Camera camera;
+    // BoxCollider object which stops camera from clipping into walls.
+    public BoxCollider collider;
+
+    // Camera pitch & yaw
+    private float pitch = 0.0f;
+    private float yaw = 0.0f;
+    void Start()
+    {
+        collider.transform.parent = camera.transform;
+        farClipPlaneDistance = camera.farClipPlane;
+        Cursor.lockState = CursorLockMode.Locked;
+        Debug.Log("Camera starting...");
+    }
+
+    void Update()
+    {
+        yaw += Input.GetAxis("Mouse X");
+        pitch -= Input.GetAxis("Mouse Y");
+        camera.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        // Get input and move camera position
+        if (Input.GetButton(forwardBtnName))
+        {
+            camera.transform.position = camera.transform.position + camera.transform.forward * moveSpeed;
+        }                                                                                               
+        if (Input.GetButton(backwardBtnName))                                                           
+        {                                                                                               
+            camera.transform.position = camera.transform.position - camera.transform.forward * moveSpeed;
+        }                                                                                               
+        if (Input.GetButton(leftBtnName))                                                               
+        {                                                                                               
+            camera.transform.position = camera.transform.position - camera.transform.right * moveSpeed;
+        }                                                                                               
+        if (Input.GetButton(rightBtnName))                                                              
+        {                                                                                               
+            camera.transform.position = camera.transform.position + camera.transform.right * moveSpeed;
+        }
+    }
+}
---
+++ Assets/Scenes/Cube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public GameObject obj;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the cube around all axis
+        obj.transform.Rotate(new Vector3(1f, Mathf.Sin(obj.transform.rotation.x % Mathf.PI * 2), Mathf.Cos(obj.transform.rotation.y % Mathf.PI * 2)));
+    }
+}
